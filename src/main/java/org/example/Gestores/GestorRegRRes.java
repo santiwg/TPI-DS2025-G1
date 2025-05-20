8583a2d7ec694595a1eea0d8915041a19641c801
Valentín <correavale2004@gmail.com>
2025-05-20 17:30:07 -0300

    refactorizo métodos y agrego conexion para obtener empleado logueado

--- src/main/java/org/example/Gestores/GestorRegRRes.java
+++ src/main/java/org/example/Gestores/GestorRegRRes.java
@@ -1,40 +1,43 @@
 package org.example.Gestores;
 
+import org.example.Entidades.Empleado;
 import org.example.Entidades.Estado;
 import org.example.Entidades.EventoSismico;
+import org.example.Entidades.Sesion;
 
 import java.util.ArrayList;
 
 public class GestorRegRRes {
 
-    private ArrayList<EventoSismico> listaEventosSismicos;
+    private ArrayList<EventoSismico> listaEventosSismicosNoRevisados;
     private EventoSismico eventoSismicoSeleccionado;
     private Estado estadoBloqueado;
     private Estado estadoRechazado;
     private ArrayList<> listaDatosEventos;
+    private Sesion sesion;
+    private Empleado empleadoLogueado;
 
     public void nuevaRevisionES(){
         this.buscarESNoRevisados();
-        this.ordenarEventosSismicos();
+        this.ordenarEventosSismicosPorFechaYHora();
         //pantalla.mostrarESParaSeleccion
     }
     public void buscarESNoRevisados(){
-        for (EventoSismico evento : eventos) {
-            if (evento.esAutoDetectado()){
-                listaEventosSismicos.add(evento);
-                listaDatosEventos.add(evento.getDatosPrincipales());
-            }
-            else if(evento.esPendienteDeRevision()){
-                listaEventosSismicos.add(evento);
-                listaDatosEventos.add(evento.getDatosPrincipales());
+        for (EventoSismico evento : eventos) { // ¿Cómo consigue todos los eventos sísmicos?¿Debería tener un atributo de seteo en el main (listaEventosSismicos)?
+            if (evento.esAutoDetectado() || evento.esPendienteDeRevision()){
+                listaEventosSismicosNoRevisados.add(evento);
+                listaDatosEventos.add(evento.getDatosPrincipales()); // Habría que unificar ambos en un diccionario.
             }
         }
     }
-    public ArrayList<> ordenarEventosSismicos(){
+    public ArrayList<> ordenarEventosSismicosPorFechaYHora(){ // ¿Debería ser un procedimiento o devolver los eventos ordenados?
 
     }
     public void tomarSeleccionES(){
 
+    }
 
+    public void buscarEmpleadoLogueado() {
+        this.empleadoLogueado = this.sesion.getEmpleadoLogueado();
     }
 }
